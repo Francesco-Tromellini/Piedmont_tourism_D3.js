c1e4cf9618feaf3d26426fbc5dae9732dc87c382
Francesco Tromellini <63305263+Francesco-Tromellini@users.noreply.github.com>
2022-01-13 17:36:49 +0100

    First files added
    Adding the first files still work in progress. The first two visualisations are already present: the comparison between italian and foreign arrivals and the global trend of Piedmont's tourism.

---
+++ main.js
@@ -0,0 +1,172 @@
+
+// Global variables initialisation
+const margin = { top: 30, right: 20, bottom: 35, left: 40 };
+const viz2 = d3.select('#viz2');
+const viz1 = d3.select('#viz1');
+const viz3 = d3.select('#viz3');
+const width = 500,
+      height = 600;
+
+const newLocal = 'position';
+// Title
+d3.select('#first_cont')
+    .html('<center><h1>Piedmont Tourism Data 2005 - 2021</h1></center>')
+    .style('background', 'whiteSmoke' )
+
+// Global data initialisation
+let region_list = ['NO', 'VC', 'BI', 'VB', 'CN', 'AL', 'TO', 'AT']
+let year_list = [2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]
+
+
+function loadData() {
+    d3.dsv(';','Piedmont_tourism_data.csv', function(d){
+        return{
+            year : d.anno,
+            region : d.provincia,
+            qualif : d.qualifica,
+            it_arr : d.arrivi_italiani,
+            it_pres : d.presenza_italiani,
+            st_arr : d.arrivi_stranieri,
+            st_pres : d.presenza_stranieri,
+            tot_arr : d.arrivi_totali,
+            tot_dep : d.partenza_totali
+        }
+    }).then(onDataLoaded);
+}
+
+// Loading data
+function onDataLoaded(data) {
+
+    let itArrForYear = [],
+        stArrForYear = [],
+        totArrForYear = [];
+
+    for(let i = 0; i < year_list.length; i++){
+        itArrForYear.push(itYearFilter(data, year_list[i]));
+        stArrForYear.push(stYearFilter(data, year_list[i]));
+        totArrForYear.push(totYearFilter(data, year_list[i]));
+    }
+
+    char1(itArrForYear, viz1);
+    char1(stArrForYear, viz2);
+    char1(totArrForYear, viz3);
+}
+
+function itYearFilter (data, currentYear) {
+
+    let data_x = data.filter(d => d.year == currentYear);
+    let result = 0;
+
+
+    for(let i = 0; i < data_x.length; i++){
+        result += Number(data_x[i].it_arr);
+    }
+
+    return result;
+}
+
+function stYearFilter (data, currentYear) {
+    let data_x = data.filter(d => d.year == currentYear);
+    let result = 0;
+
+
+    for(let i = 0; i < data_x.length; i++){
+        if(data_x[i].st_arr == 'null'){
+            data_x[i].st_arr = 0;
+        }
+        result += Number(data_x[i].st_arr);
+    }
+
+    return result;
+}
+
+function totYearFilter (data, currentYear) {
+    let data_x = data.filter(d => d.year == currentYear);
+    let result = 0;
+
+
+    for(let i = 0; i < data_x.length; i++){
+        if(data_x[i].tot_arr == 'null'){
+            data_x[i].tot_arr = 0;
+        }
+        result += Number(data_x[i].tot_arr);
+    }
+
+    return result;
+}
+
+function char1(charData, pointer) {
+
+    console.log(charData);
+
+    const svg = pointer.append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .style('background', 'whiteSmoke')
+
+    let yScale,
+        xScale,
+        colors;
+
+    yScale = d3.scaleLinear()
+        .domain([0, d3.max(charData)])
+        .range([height - margin.bottom - 5, margin.top])
+        .interpolate(d3.interpolateRound);
+
+    yAxisValues = d3.scaleLinear()
+        .domain([0, d3.max(charData)])
+        .range([height - margin.bottom - 5, margin.top])
+        .interpolate(d3.interpolateRound);
+
+    yAxisTicks = d3.axisLeft(yAxisValues)
+        .ticks(9)
+        .tickFormat(d => `${d/1000} K`)
+
+    xScale = d3.scaleBand()
+        .domain(charData)
+        .padding(0.1)
+        .range([0, width - margin.left - margin.right ]);
+
+    xAxisValues = d3.scaleBand()
+        .domain(year_list)
+        .range([margin.left, width - margin.right]);
+
+    colors = d3.scaleSequential()
+        .domain([d3.min(charData), d3.max(charData)])
+        .interpolator(d3.interpolateBlues);
+
+    
+    svg.append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.right + ')')
+        .style('background', '#F5F5F5')
+        .selectAll('rect').data(charData)
+            .enter().append('rect')
+                .style('fill', colors)
+                .attr('width', xScale.bandwidth())
+                .attr('height', d => yScale(0) - yScale(d))
+                .attr('x', d => xScale(d))
+                .attr('y', d => yScale(d));
+
+
+    svg.append('g')
+        .attr('transform', `translate(${margin.left}, 18)`)
+        .call(yAxisTicks)
+        .call(g => g.select('.domain').remove());
+
+    svg.append('g')
+        .attr('transform', `translate(0, ${height - margin.bottom +15})`)
+        .call(d3.axisBottom(xAxisValues))
+        .call(g => g.select('.domain').remove());
+
+    svg.append('text')
+        .attr('font-size', '21px')
+        .attr('class', 'title')
+        .attr('x', width / 2 + margin.left / 2)
+        .attr('y', 20)
+        .attr('text-anchor', 'middle')
+        .text('Number of arrivals per year')
+        .style('fill', 'black')
+
+}
+
+loadData();
